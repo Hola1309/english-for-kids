dbd339a79db9cc6afdc1ad5034b6bbd8c33a81b0
Hola <hola131098@gmail.com>
2020-04-21 22:06:35 +0400

    feat: new rep for gh-pages

---
+++ index.js
@@ -0,0 +1,455 @@
+
+
+var animals = {
+    slide0:{
+        src:"./assets/boar.jpg",
+        senceEN:'Boar',
+        senceRU:'Кабан'
+    },
+    slide1:{
+        src:"./assets/cat.jpg",
+        senceEN:'Cat',
+        senceRU:'Кошка'
+    },
+    slide2:{
+        src:"./assets/cow.jpg",
+        senceEN:'Cow',
+        senceRU:'Корова'
+    },
+    slide3:{
+        src:"./assets/dog.jpg",
+        senceEN:'Dog',
+        senceRU:'Собака'
+    },
+    slide4:{
+        src:"./assets/horse.jpg",
+        senceEN:'Horse',
+        senceRU:'Лошадь'
+    },
+    slide5:{
+        src:"./assets/rabbit.jpg",
+        senceEN:'Rabbit',
+        senceRU:'Кролик'
+    },
+    slide6:{
+        src:"./assets/rat.jpg",
+        senceEN:'Rat',
+        senceRU:'Крыса'
+    },
+    slide7:{
+        src:"./assets/tiger.jpg",
+        senceEN:'Tiger',
+        senceRU:'Тигр'
+    }
+}
+var family = {
+    slide0:{
+        src:"./assets/grandfather.jpg",
+        senceEN:'grandfather',
+        senceRU:'Дедушка'
+    },
+    slide1:{
+        src:"./assets/grandmother.jpg",
+        senceEN:'grandmother',
+        senceRU:'Бабушка'
+    },
+    slide2:{
+        src:"./assets/parents.jpg",
+        senceEN:'parents',
+        senceRU:'Родители'
+    },
+    slide3:{
+        src:"./assets/sister.jpg",
+        senceEN:'sister',
+        senceRU:'Сестра'
+    },
+    slide4:{
+        src:"./assets/brother.jpg",
+        senceEN:'brother',
+        senceRU:'Брат'
+    },
+    slide5:{
+        src:"./assets/dad.jpg",
+        senceEN:'dad',
+        senceRU:'Папа'
+    },
+    slide6:{
+        src:"./assets/mom.jpg",
+        senceEN:'mom',
+        senceRU:'Мама'
+    },
+    slide7:{
+        src:"./assets/children.jpg",
+        senceEN:'children',
+        senceRU:'Дети'
+    }
+}
+var clothes = {
+    slide0:{
+        src:"./assets/blouse.jpg",
+        senceEN:'blouse',
+        senceRU:'Дедушка'
+    },
+    slide1:{
+        src:"./assets/tie.jpg",
+        senceEN:'tie',
+        senceRU:'Бабушка'
+    },
+    slide2:{
+        src:"./assets/jeans.jpg",
+        senceEN:'jeans',
+        senceRU:'Родители'
+    },
+    slide3:{
+        src:"./assets/suit.jpg",
+        senceEN:'suit',
+        senceRU:'Сестра'
+    },
+    slide4:{
+        src:"./assets/jacket.jpg",
+        senceEN:'jacket',
+        senceRU:'Брат'
+    },
+    slide5:{
+        src:"./assets/sock.jpg",
+        senceEN:'sock',
+        senceRU:'Папа'
+    },
+    slide6:{
+        src:"./assets/shirt.jpg",
+        senceEN:'shirt',
+        senceRU:'Мама'
+    },
+    slide7:{
+        src:"./assets/scarf.jpg",
+        senceEN:'scarf',
+        senceRU:'Дети'
+    }
+}
+var food = {
+    slide0:{
+        src:"./assets/omelette.jpg",
+        senceEN:'omelette',
+        senceRU:'Дедушка'
+    },
+    slide1:{
+        src:"./assets/pancakes.jpg",
+        senceEN:'pancakes',
+        senceRU:'Бабушка'
+    },
+    slide2:{
+        src:"./assets/wine.jpg",
+        senceEN:'wine',
+        senceRU:'Родители'
+    },
+    slide3:{
+        src:"./assets/biscuits.jpg",
+        senceEN:'biscuits',
+        senceRU:'Сестра'
+    },
+    slide4:{
+        src:"./assets/cream.jpg",
+        senceEN:'cream',
+        senceRU:'Брат'
+    },
+    slide5:{
+        src:"./assets/tea.jpg",
+        senceEN:'tea',
+        senceRU:'Папа'
+    },
+    slide6:{
+        src:"./assets/soup.jpg",
+        senceEN:'soup',
+        senceRU:'Мама'
+    },
+    slide7:{
+        src:"./assets/sausages.jpg",
+        senceEN:'sausages',
+        senceRU:'Дети'
+    }
+}
+var colors  = {
+    slide0:{
+        src:"./assets/Red.jpg",
+        senceEN:'Red',
+        senceRU:'Дедушка'
+    },
+    slide1:{
+        src:"./assets/Green.jpg",
+        senceEN:'Green',
+        senceRU:'Бабушка'
+    },
+    slide2:{
+        src:"./assets/Blue.jpg",
+        senceEN:'Blue',
+        senceRU:'Родители'
+    },
+    slide3:{
+        src:"./assets/Orange.jpg",
+        senceEN:'Orange',
+        senceRU:'Сестра'
+    },
+    slide4:{
+        src:"./assets/Yellow.jpg",
+        senceEN:'Yellow',
+        senceRU:'Брат'
+    },
+    slide5:{
+        src:"./assets/Pink.jpg",
+        senceEN:'Pink',
+        senceRU:'Папа'
+    },
+    slide6:{
+        src:"./assets/Grey.jpg",
+        senceEN:'Grey',
+        senceRU:'Мама'
+    },
+    slide7:{
+        src:"./assets/Black.jpg",
+        senceEN:'Black',
+        senceRU:'Дети'
+    }
+}
+var house  = {
+    slide0:{
+        src:"./assets/Bedroom.jpg",
+        senceEN:'Bedroom',
+        senceRU:'Дедушка'
+    },
+    slide1:{
+        src:"./assets/Kitchen.jpg",
+        senceEN:'Kitchen',
+        senceRU:'Бабушка'
+    },
+    slide2:{
+        src:"./assets/Bathroom.jpg",
+        senceEN:'Bathroom',
+        senceRU:'Родители'
+    },
+    slide3:{
+        src:"./assets/Table.jpg",
+        senceEN:'Table',
+        senceRU:'Сестра'
+    },
+    slide4:{
+        src:"./assets/Chair.jpg",
+        senceEN:'Chair',
+        senceRU:'Брат'
+    },
+    slide5:{
+        src:"./assets/Sofa.jpg",
+        senceEN:'Sofa',
+        senceRU:'Папа'
+    },
+    slide6:{
+        src:"./assets/Mirror.jpg",
+        senceEN:'Mirror',
+        senceRU:'Мама'
+    },
+    slide7:{
+        src:"./assets/Towel.jpg",
+        senceEN:'Towel',
+        senceRU:'Дети'
+    }
+}
+var Weather  = {
+    slide0:{
+        src:"./assets/Sun.jpg",
+        senceEN:'Sun',
+        senceRU:'Дедушка'
+    },
+    slide1:{
+        src:"./assets/Rain.jpg",
+        senceEN:'Rain',
+        senceRU:'Бабушка'
+    },
+    slide2:{
+        src:"./assets/Cloud.jpg",
+        senceEN:'Cloud',
+        senceRU:'Родители'
+    },
+    slide3:{
+        src:"./assets/Snow.jpg",
+        senceEN:'Snow',
+        senceRU:'Сестра'
+    },
+    slide4:{
+        src:"./assets/Fog.jpg",
+        senceEN:'Fog',
+        senceRU:'Брат'
+    },
+    slide5:{
+        src:"./assets/Spring.jpg",
+        senceEN:'Spring',
+        senceRU:'Папа'
+    },
+    slide6:{
+        src:"./assets/Mirror.jpg",
+        senceEN:'Summer',
+        senceRU:'Мама'
+    },
+    slide7:{
+        src:"./assets/Autumn.jpg",
+        senceEN:'Autumn',
+        senceRU:'Дети'
+    }
+}
+var Body  = {
+    slide0:{
+        src:"./assets/Head.jpg",
+        senceEN:'Head',
+        senceRU:'Дедушка'
+    },
+    slide1:{
+        src:"./assets/Hair.jpg",
+        senceEN:'Hair',
+        senceRU:'Бабушка'
+    },
+    slide2:{
+        src:"./assets/Eyes.jpg",
+        senceEN:'Eyes',
+        senceRU:'Родители'
+    },
+    slide3:{
+        src:"./assets/Nose.jpg",
+        senceEN:'Nose',
+        senceRU:'Сестра'
+    },
+    slide4:{
+        src:"./assets/Teeth.jpg",
+        senceEN:'Teeth',
+        senceRU:'Брат'
+    },
+    slide5:{
+        src:"./assets/Lips.jpg",
+        senceEN:'Lips',
+        senceRU:'Папа'
+    },
+    slide6:{
+        src:"./assets/Ears.jpg",
+        senceEN:'Ears',
+        senceRU:'Мама'
+    },
+    slide7:{
+        src:"./assets/Neck.jpg",
+        senceEN:'Neck',
+        senceRU:'Дети'
+    }
+}
+var Start  = {
+    slide0:{
+        src:"./assets/Body.jpg",
+        senceEN:'1',
+        senceRU:'Дедушка'
+    },
+    slide1:{
+        src:"./assets/Weather.jpg",
+        senceEN:'2',
+        senceRU:'Бабушка'
+    },
+    slide2:{
+        src:"./assets/house.jpg",
+        senceEN:'3',
+        senceRU:'Родители'
+    },
+    slide3:{
+        src:"./assets/colors.jpg",
+        senceEN:'4',
+        senceRU:'Сестра'
+    },
+    slide4:{
+        src:"./assets/food.jpg",
+        senceEN:'5',
+        senceRU:'Брат'
+    },
+    slide5:{
+        src:"./assets/clothes.jpg",
+        senceEN:'6',
+        senceRU:'Папа'
+    },
+    slide6:{
+        src:"./assets/family.jpg",
+        senceEN:'7',
+        senceRU:'Мама'
+    },
+    slide7:{
+        src:"./assets/animals.jpg",
+        senceEN:'8',
+        senceRU:'Дети'
+    }
+}
+var lessons = [Start, Body, Weather, house, colors, food, clothes, family, animals]
+var test=false;
+var j = 0;
+var numOfLesson = 0;
+var cards = [ lessons[numOfLesson].slide0, lessons[numOfLesson].slide1, lessons[numOfLesson].slide2, lessons[numOfLesson].slide3, lessons[numOfLesson].slide4, lessons[numOfLesson].slide5, lessons[numOfLesson].slide6, lessons[numOfLesson].slide7]
+
+
+function training(){
+    test = true;
+    
+    for (var i = cards.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = cards[i];
+        cards[i] = cards[j];
+        cards[j] = temp;
+    }
+    for (i=0; i<8;i++){
+        
+        document.getElementById('allcards').querySelectorAll('span')[i].innerText = '';
+        
+    }
+    errors = 0;
+    j=0;
+    message = cards[0].senceEN;
+    speak();
+    return test,j;
+}
+
+function render(){
+for (i=0; i<8;i++){
+    cards = [ lessons[numOfLesson].slide0, lessons[numOfLesson].slide1, lessons[numOfLesson].slide2, lessons[numOfLesson].slide3, lessons[numOfLesson].slide4, lessons[numOfLesson].slide5, lessons[numOfLesson].slide6, lessons[numOfLesson].slide7]
+    document.getElementById('allcards').querySelectorAll('img')[i].src = cards[i].src;
+    document.getElementById('allcards').querySelectorAll('img')[i].className = cards[i].senceEN;
+    document.getElementById('allcards').querySelectorAll('span')[i].innerText = cards[i].senceEN;
+    
+}
+}
+render()
+
+document.getElementById('allcards').addEventListener('click', (e) =>{
+    
+    if(numOfLesson==0){
+        numOfLesson = +e.target.className
+        render()
+    }
+    
+    var check = e.target.className
+    console.log(check)  
+      
+    if (test == true){
+        
+        if(check == message){
+            
+            j++;
+            if (j == 8){
+                message=''
+                j=0;
+                test = false
+                return alert('Training complete!' + '\n\nОшибок:' + errors);
+                
+            }
+            message = cards[j].senceEN;
+            speak();
+            
+        } else {
+            errors++
+            document.getElementById('count').innerText=errors
+        }
+    }
+})
+
+
+function speak(){
+    speechSynthesis.speak(new SpeechSynthesisUtterance(message))
+}
